e8f615feede56a6d0bf1e30fcda4ddcd8a43b756
Vishnu Divakar <vishnudivakar31@gmail.com>
2020-05-17 09:09:41 -0400

    code initial commit

---
+++ Insight/src/main/java/io/vdlabz/Insight.java
@@ -0,0 +1,27 @@
+package io.vdlabz;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+public class Insight {
+    public static void main(String[] args) {
+        Logger.getLogger("org.apache").setLevel(Level.WARN);
+
+        SparkSession spark = SparkSession
+                .builder()
+                .master("local[*]")
+                .appName(Insight.class.getName())
+                .getOrCreate();
+
+        Dataset<Row> csvData = spark
+                .read()
+                .option("inferSchema", true)
+                .option("header", true)
+                .csv("src/main/resources/*");
+
+        csvData.show();
+    }
+}
